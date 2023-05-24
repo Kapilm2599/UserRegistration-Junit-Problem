56ebd3bfb5d6924d09dbc4fc94dbe89e07b3f2fb
Kapilm2599 <mahajankapil575@gmail.com>
2023-05-24 15:47:06 +0530

    Added: Refactored code custom exception

--- src/main/java/com/blz/junit/InvalidUserInputException.java
+++ src/main/java/com/blz/junit/InvalidUserInputException.java
@@ -3,75 +3,104 @@ package com.blz.junit;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class UserRegistrationTest {
-
-    @Test
+public class InvalidUserInputException extends Exception {
+	@Test
     public void givenFirstName_whenProper_ShouldReturnTrue() {
-        UserRegistration userRegistrationRegEx = new UserRegistration();
-        boolean result = userRegistrationRegEx.firstNameValidate("Kapil");
-        Assert.assertTrue(result);
+        try {
+        	UserRegistration userRegistrationRegEx = new UserRegistration();
+            boolean result = userRegistrationRegEx.firstNameValidate("Kapil");
+            Assert.assertTrue(result);
+        } catch (Exception e) {
+        }
     }
 
     @Test
     public void givenFirstName_whenNotProper_ShouldReturnFalse() {
-    	UserRegistration userRegistrationRegEx = new UserRegistration();
+        try{
+        	UserRegistration userRegistrationRegEx = new UserRegistration();
         boolean result = userRegistrationRegEx.firstNameValidate("kapil");
         Assert.assertFalse(result);
-    }
+    } catch (Exception e) {
+        }
+        }
 
     @Test
     public void givenLastName_whenProper_ShouldReturnTrue() {
-    	UserRegistration userRegistrationRegEx = new UserRegistration();
+        try{
+        	UserRegistration userRegistrationRegEx = new UserRegistration();
         boolean result = userRegistrationRegEx.lastNameValidate("Mahajan");
         Assert.assertTrue(result);
+    } catch (Exception e) {
+    }
     }
 
     @Test
     public void givenLastName_whenNotProper_ShouldReturnFalse() {
-    	UserRegistration userRegistrationRegEx = new UserRegistration();
+        try{
+        	UserRegistration userRegistrationRegEx = new UserRegistration();
         boolean result = userRegistrationRegEx.lastNameValidate("mahajan");
         Assert.assertFalse(result);
+    } catch (Exception e) {
+        }
     }
 
     @Test
     public void givenEmail_whenProper_ShouldReturnTrue() {
-    	UserRegistration userRegistrationRegEx = new UserRegistration();
+        try{
+        	UserRegistration userRegistrationRegEx = new UserRegistration();
         boolean result = userRegistrationRegEx.emailValidate("abc.xyz@bl.co.in");
         Assert.assertTrue(result);
+        } catch (Exception e) {
+        }
     }
 
     @Test
     public void givenEmail_whenNotProper_ShouldReturnFalse() {
-    	UserRegistration userRegistrationRegEx = new UserRegistration();
+        try{
+        	UserRegistration userRegistrationRegEx = new UserRegistration();
         boolean result = userRegistrationRegEx.emailValidate("abc.xyz@bl.in");
         Assert.assertFalse(result);
+        } catch (Exception e) {
+        }
     }
 
     @Test
     public void givenPhone_whenProper_ShouldReturnTrue() {
-    	UserRegistration userRegistrationRegEx = new UserRegistration();
+        try{
+        	UserRegistration userRegistrationRegEx = new UserRegistration();
         boolean result = userRegistrationRegEx.phoneNumberValidate("91 7879219494");
         Assert.assertTrue(result);
+        } catch (Exception e) {
+        }
     }
 
     @Test
     public void givenPhone_whenNotProper_ShouldReturnFalse() {
-    	UserRegistration userRegistrationRegEx = new UserRegistration();
+        try{
+        	UserRegistration userRegistrationRegEx = new UserRegistration();
         boolean result = userRegistrationRegEx.phoneNumberValidate("7879219494");
         Assert.assertFalse(result);
+        } catch (Exception e) {
+        }
     }
 
     @Test
     public void givenPassword_whenProper_ShouldReturnTrue() {
-    	UserRegistration userRegistrationRegEx = new UserRegistration();
+        try{
+        	UserRegistration userRegistrationRegEx = new UserRegistration();
         boolean result = userRegistrationRegEx.passwordValidate("Abc12345#");
         Assert.assertTrue(result);
+        } catch (Exception e) {
+        }
     }
 
     @Test
     public void givenPassword_whenNotProper_ShouldReturnFalse() {
-    	UserRegistration userRegistrationRegEx = new UserRegistration();
-        boolean result = userRegistrationRegEx.passwordValidate("kapil23#%");
+        try{
+        	UserRegistration userRegistrationRegEx = new UserRegistration();
+        boolean result = userRegistrationRegEx.passwordValidate("Kapil23#%");
         Assert.assertFalse(result);
+        } catch (Exception e) {
+        }
     }
 }
